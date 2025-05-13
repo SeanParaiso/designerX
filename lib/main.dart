e8410cb97239fb745d24e5892c0eba4164fb6d7c
SeanParaiso <seanharvey.paraiso.cics@ust.edu.ph>
2025-05-13 21:20:45 +0800

    added firebase config para gumana Chrome (web) etc.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart'; // Import the generated configuration file
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // Use the generated options
+  );
   runApp(const ArtDesignApp());
 }
 
